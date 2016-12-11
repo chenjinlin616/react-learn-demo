f754f7428c549e817c7d0d0a1fc02ac27cd47d30
gjincai <1262868349@qq.com>
2016-12-11 22:30:54 +0800

    +1

--- react-router-demo1/app/components/Index.js
+++ react-router-demo1/app/components/Index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import testPng from './../img/pay-fail.png';
 
 export default class Index extends React.Component{
     render(){
         return(
             <div className='news'>
                 Index.js
+                <img src={testPng}/>
             </div>
         )
     }
--- react-router-demo1/app/components/NavBot.js
+++ react-router-demo1/app/components/NavBot.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router'
-import './../scss/navBot.scss'
+import React from 'react';
+import { Link } from 'react-router';
+import './../scss/navBot.scss';
 
 export default class NavBot extends React.Component{
     render(){
+        // console.log(this.props.children);
         return(
-            <div className='nav-bot'>
-                <li><Link to="/">index</Link></li>
-                <li><Link to="/news">news</Link></li>
-                <li><Link to="/find">find</Link></li>
-                <li><Link to="/mine">mine</Link></li>
+            <div>
+                <div className='nav-bot'>
+                    <li><Link to="/">index</Link></li>
+                    <li><Link to="/news">news</Link></li>
+                    <li><Link to="/find">find</Link></li>
+                    <li><Link to="/mine">mine</Link></li>
+                </div>
                 {this.props.children}
             </div>
         )
