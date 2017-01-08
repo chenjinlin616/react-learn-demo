96f6c5ac35928eea7bd0af6d7373291eee671610
gjincai <1262868349@qq.com>
2017-01-09 00:07:23 +0800

    route嵌套

---
+++ react-router-demo1/app/components/Mine_Center.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Mine_Center extends React.Component{
+    render(){
+        return(
+            <div className='news'>
+                Mine_Center.js
+            </div>
+        )
+    }
+}
---
+++ react-router-demo1/app/components/Mine_Help.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Mine_Help extends React.Component{
+    render(){
+        return(
+            <div className='news'>
+                Mine_Help.js
+            </div>
+        )
+    }
+}
